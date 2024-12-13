a76adb85f36d183ab46cdaebe875357e8ae147b9
root <iauyeung@nvidia.com>
2024-12-13 06:42:34 +0000

    Revert "Fix hpx dataloader setting channel not supporting one gpu"
    This reverts commit ff3c4191f61f1e64c99573a4f9bd36e4e1d9bf02.

--- modulus/datapipes/healpix/data_modules.py
+++ modulus/datapipes/healpix/data_modules.py
@@ -565,6 +565,11 @@ class TimeSeriesDataModule:
                     constants=self.constants is not None,
                     batch_size=self.batch_size,
                 )
+
+                dataset = dataset.sel(
+                    channel_in=self.input_variables,
+                    channel_out=self.output_variables,
+                )
             else:
                 dataset = open_fn(
                     input_variables=self.input_variables,
@@ -606,10 +611,6 @@ class TimeSeriesDataModule:
                     batch_size=self.batch_size,
                 )
 
-        dataset = dataset.sel(
-            channel_in=self.input_variables,
-            channel_out=self.output_variables,
-        )
         if self.constants is not None:
             dataset = dataset.sel(channel_c=list(self.constants.values()))
 
@@ -987,6 +988,11 @@ class CoupledTimeSeriesDataModule(TimeSeriesDataModule):
                     constants=self.constants is not None,
                     batch_size=self.batch_size,
                 )
+
+                dataset = dataset.sel(
+                    channel_in=self.input_variables + coupled_variables,
+                    channel_out=self.output_variables,
+                )
             else:
                 dataset = open_fn(
                     input_variables=self.input_variables + coupled_variables,
@@ -1028,10 +1034,6 @@ class CoupledTimeSeriesDataModule(TimeSeriesDataModule):
                     batch_size=self.batch_size,
                 )
 
-        dataset = dataset.sel(
-            channel_in=self.input_variables + coupled_variables,
-            channel_out=self.output_variables,
-        )
         if self.constants is not None:
             dataset = dataset.sel(channel_c=list(self.constants.values()))
 
